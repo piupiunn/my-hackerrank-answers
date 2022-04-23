a1f64bcce6d4d90b01ed3fa16ef97bdbf5dc0023
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-04-23 21:06:03 +0300

    up

--- algorithms/easy/cats-and-a-mouse.js
+++ algorithms/easy/cats-and-a-mouse.js
@@ -2,10 +2,16 @@
   https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true
 }
 
-https: function catAndMouse(x, y, z) {
-  return Math.abs(x - z) > Math.abs(y - z)
-    ? "Cat B"
-    : Math.abs(x - z) < Math.abs(y - z)
-    ? "Cat A"
-    : "Mouse C";
+position x for cat A
+position y for cat B
+position z for mouse C,
+  
+
+ function catAndMouse(x, y, z) {
+   return Math.abs(x - z) > (y - z) ? "Cat B"
+     : Math.abs(y - z) > (x - z) ? "Cat A"
+       : "Mouse C"
+   
 }
+
+
---
+++ algorithms/easy/hurdle-race.js
@@ -0,0 +1,8 @@
+{
+    https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true
+}
+
+function hurdleRace(k, height) {
+    return Math.max(...height, k) - k
+
+}
